5e670a926a9d84f5e6e6cc897cb2ff4860d62436
Le-Huyen-Trang <huyentrang10m@gmail.com>
2025-02-21 10:18:21 +0700

    script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function login()
+{
+  let u = usernam.value
+  let p = pw.value
+  if (u=="trang" && p=="0812")
+  {
+    window.open("home.html")
+    documet.getElementById(compliler).style.display="block"
+  }
+}  
